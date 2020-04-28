e8987203ee6fa52bd412a2ae93832167295cad16
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-04-28 02:02:42 +0100

    added documentation on class TraceabilityInfo

--- fabric-samples/chaincode/fabcar/java/src/main/java/org/hyperledger/fabric/samples/fabcar/DataTypes/TraceabilityInfo.java
+++ fabric-samples/chaincode/fabcar/java/src/main/java/org/hyperledger/fabric/samples/fabcar/DataTypes/TraceabilityInfo.java
@@ -6,15 +6,33 @@ import org.hyperledger.fabric.contract.annotation.Property;
 
 import java.util.ArrayList;
 
+/**
+ * This class represents an entry of traceability information.
+ * This is the base class for traceability information.
+ * Sub classes are used where there are necessary additional attributes, depending on the state of validation of the traceability information.
+ * Please check the TraceabilityInformationStateMachine package for more information.
+ */
 @DataType()
 public class TraceabilityInfo
 {
+    /**
+     * The hash value of the input dataset used to perform the data transformation.
+     * This is used to validate the integrity of the input dataset, in order to be able to verify the traceability information.
+     */
     @Property()
     protected final String inputDatasetHashValue;
 
+    /**
+     * The hash value of the output dataset used to perform the data transformation.
+     * This is used to validate the integrity of the input dataset, in order to be able to verify the traceability information.
+     */
     @Property()
     protected final String outputDatasetHashValue;
 
+    /**
+     * This is an instance of the class ProcessingDetails which contains information regarding the steps taken to perform the data transformation.
+     * These steps are necessary in order to check the veracity of the traceability information.
+     */
     @Property()
     protected final ProcessingDetails processingDetails;
 
