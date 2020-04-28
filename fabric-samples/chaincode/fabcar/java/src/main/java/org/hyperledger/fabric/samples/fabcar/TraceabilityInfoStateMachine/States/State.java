4dde27d842afbf682863e35b6fa1d3823126e612
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-04-28 21:23:07 +0100

    continued class State

--- fabric-samples/chaincode/fabcar/java/src/main/java/org/hyperledger/fabric/samples/fabcar/TraceabilityInfoStateMachine/States/State.java
+++ fabric-samples/chaincode/fabcar/java/src/main/java/org/hyperledger/fabric/samples/fabcar/TraceabilityInfoStateMachine/States/State.java
@@ -1,11 +1,21 @@
 package org.hyperledger.fabric.samples.fabcar.TraceabilityInfoStateMachine.States;
 
+import org.hyperledger.fabric.samples.fabcar.DataTypes.Entity;
+import org.hyperledger.fabric.samples.fabcar.DataTypes.TraceabilityInfo;
+
 /**
  * This is the base class for a state machine for the traceability information.
  * Subclasses of this class implement the specific actions for each state of the traceability information.
  * Since all the information must be saved on the blockchain, the data classes are well split from the logic classes.
  * This class is called to perform actions based on the state of the traceability information.
  */
-public class State
+public abstract class State
 {
+
+    public abstract void voteYesForTheVeracityOfTraceabilityInfo(TraceabilityInfo traceabilityInfo, Entity voter);
+
+    public abstract void voteNoForTheVeracityOfTraceabilityInfo(TraceabilityInfo traceabilityInfo, Entity voter);
+
+    public abstract void flagTraceabilityInfoAsFalse(TraceabilityInfo traceabilityInfo, Entity whistleblower);
+
 }
