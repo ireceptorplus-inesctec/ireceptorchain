0806eaa7015005e8eea906660377c3b779ed3ef4
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-01 21:50:35 +0100

    hard coded java as default language in startFabric.sh

--- fabric-samples/fabcar/startFabric.sh
+++ fabric-samples/fabcar/startFabric.sh
@@ -21,8 +21,6 @@ if [ "$CC_SRC_LANGUAGE" != "go" -a "$CC_SRC_LANGUAGE" != "golang" -a "$CC_SRC_LA
 
 fi
 
-$CC_SRC_LANGUAGE = "java"; #JAVA FTW
-
 # clean out any old identites in the wallets
 rm -rf javascript/wallet/*
 rm -rf java/wallet/*
@@ -32,7 +30,7 @@ rm -rf typescript/wallet/*
 pushd ../test-network
 ./network.sh down
 ./network.sh up createChannel -ca -s couchdb
-./network.sh deployCC -l ${CC_SRC_LANGUAGE}
+./network.sh deployCC -l java
 popd
 
 cat <<EOF
