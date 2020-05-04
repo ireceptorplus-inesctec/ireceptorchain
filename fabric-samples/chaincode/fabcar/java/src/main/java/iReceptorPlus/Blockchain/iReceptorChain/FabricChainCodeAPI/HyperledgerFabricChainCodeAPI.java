5aa5d7540c5a9b2f102e24fafa7f67b1e110b9c5
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-04 01:30:57 +0100

    continued class HyperledgerFabricChainCodeAPI

--- fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/FabricChainCodeAPI/HyperledgerFabricChainCodeAPI.java
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/FabricChainCodeAPI/HyperledgerFabricChainCodeAPI.java
@@ -1,6 +1,25 @@
 package iReceptorPlus.Blockchain.iReceptorChain.FabricChainCodeAPI;
 
+import org.hyperledger.fabric.contract.Context;
+
+/**
+ * This class is an API that handles all calls to the hyperledger functions.
+ * It is used to abstract complicated calls to the hyperledger API from the application logic.
+ * The class receives the blockchain context in which this class will perform the required tasks and can then be called to perform operations on that same context without the need for interacting directly with hyperledger fabric's methods.
+ */
 public class HyperledgerFabricChainCodeAPI
 {
+    /**
+     * The blockchain context in which this class will perform the required tasks.
+     */
+    private Context ctx;
 
+    /**
+     * Constructor for this class.
+     * @param ctx The blockchain context in which this class will perform the required tasks.
+     */
+    public HyperledgerFabricChainCodeAPI(Context ctx)
+    {
+        this.ctx = ctx;
+    }
 }
