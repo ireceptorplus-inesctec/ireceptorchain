e27b7cd07b475c7a4ee79cde7a350b56753f6872
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-04 01:20:34 +0100

    added class ChaincodeConfigs to contain some configs that should be the same for all nodes of the network

---
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChaincodeConfigs.java
@@ -0,0 +1,8 @@
+package iReceptorPlus.Blockchain.iReceptorChain;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class ChaincodeConfigs
+{
+    public static AtomicLong numberOfConfirmationsNecessaryForTraceabilityInfoToBeValid = new AtomicLong(3);
+}
