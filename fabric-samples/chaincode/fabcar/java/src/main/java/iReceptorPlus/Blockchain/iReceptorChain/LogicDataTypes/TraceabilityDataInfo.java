c51760163f1442184f7fa5d6816b9aff5a14ed07
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-04 02:07:27 +0100

    added constructor and getters on last commit's class

--- fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/LogicDataTypes/TraceabilityDataInfo.java
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/LogicDataTypes/TraceabilityDataInfo.java
@@ -7,10 +7,26 @@ public class TraceabilityDataInfo
     /**
      * The traceability data that this class refers to just as it is stored on chain.
      */
-    TraceabilityData traceabilityData;
+    protected TraceabilityData traceabilityData;
 
     /**
      * The key used to store the traceability data on the blockchain.
      */
-    String key;
+    protected String key;
+
+    public TraceabilityDataInfo(TraceabilityData traceabilityData, String key)
+    {
+        this.traceabilityData = traceabilityData;
+        this.key = key;
+    }
+
+    public TraceabilityData getTraceabilityData()
+    {
+        return traceabilityData;
+    }
+
+    public String getKey()
+    {
+        return key;
+    }
 }
