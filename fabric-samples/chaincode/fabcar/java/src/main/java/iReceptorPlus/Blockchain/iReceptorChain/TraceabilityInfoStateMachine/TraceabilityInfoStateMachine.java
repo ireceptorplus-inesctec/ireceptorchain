adc8b6dfcfcf53b84479cb83fa5b90c1c0b55868
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-04 01:39:47 +0100

    added attribs to class TraceabilityInfoStateMachineException and created necessary exception class

--- fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/TraceabilityInfoStateMachine/TraceabilityInfoStateMachine.java
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/TraceabilityInfoStateMachine/TraceabilityInfoStateMachine.java
@@ -1,5 +1,13 @@
 package iReceptorPlus.Blockchain.iReceptorChain.TraceabilityInfoStateMachine;
 
+import iReceptorPlus.Blockchain.iReceptorChain.DataTypes.TraceabilityInfo;
+import iReceptorPlus.Blockchain.iReceptorChain.DataTypes.TraceabilityInfoAwatingValidation;
+import iReceptorPlus.Blockchain.iReceptorChain.DataTypes.TraceabilityInfoValidated;
+import iReceptorPlus.Blockchain.iReceptorChain.TraceabilityInfoStateMachine.Exceptions.UnsupportedTypeOfTraceabilityInfo;
+import iReceptorPlus.Blockchain.iReceptorChain.TraceabilityInfoStateMachine.States.AwaitingValidation;
+import iReceptorPlus.Blockchain.iReceptorChain.TraceabilityInfoStateMachine.States.State;
+import iReceptorPlus.Blockchain.iReceptorChain.TraceabilityInfoStateMachine.States.Validated;
+
 /**
  * This class implements a state machine for the traceability information.
  * Since all the information must be saved on the blockchain, the data classes are well split from the logic classes.
@@ -7,4 +15,25 @@ package iReceptorPlus.Blockchain.iReceptorChain.TraceabilityInfoStateMachine;
  */
 public class TraceabilityInfoStateMachine
 {
+    /**
+     * The TraceabilityInfo instance where the class will operate.
+     */
+    TraceabilityInfo traceabilityInfo;
+    /**
+     * An instance of a subclass of class State that implements the required logic for the specific state that the state machine should be in.
+     * The instance is created on the constructor, based on the type of TraceabilityInfo passed as argument to the constructor.
+     */
+    State state;
+
+    public TraceabilityInfoStateMachine(TraceabilityInfo traceabilityInfo) throws UnsupportedTypeOfTraceabilityInfo
+    {
+        this.traceabilityInfo = traceabilityInfo;
+        if (traceabilityInfo instanceof TraceabilityInfoAwatingValidation)
+            state = new AwaitingValidation();
+        else if (traceabilityInfo instanceof TraceabilityInfoValidated)
+            state = new Validated();
+        else
+            throw new UnsupportedTypeOfTraceabilityInfo("The traceability information given is not supported by the state machine");
+
+    }
 }
