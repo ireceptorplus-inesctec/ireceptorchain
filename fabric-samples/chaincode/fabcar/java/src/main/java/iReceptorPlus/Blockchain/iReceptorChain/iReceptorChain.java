ae63e2e5afcd566d3b334212eb0d1eb82c5f9e00
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-05 21:47:15 +0100

    another test regarding returning data types to client

--- fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/iReceptorChain.java
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/iReceptorChain.java
@@ -256,9 +256,9 @@ public final class iReceptorChain implements ContractInterface {
         System.err.println(traceabilityInfoValidated);
         if (traceabilityInfoValidated.isEmpty())
             System.err.println("does not exist2");
-        TraceabilityData resultsValidated = genson.deserialize(traceabilityInfoValidated, TraceabilityDataAwatingValidation.class);
+        iReceptorChainDataType resultsValidated = genson.deserialize(traceabilityInfoValidated, TraceabilityDataAwatingValidation.class);
 
-        return resultsValidated;
+        return (TraceabilityData) resultsValidated;
 /*
         resultsAwatingValidation.registerYesVoteForValidity(new Entity());
         resultsValidated.registerYesVoteForValidity(new Entity());
