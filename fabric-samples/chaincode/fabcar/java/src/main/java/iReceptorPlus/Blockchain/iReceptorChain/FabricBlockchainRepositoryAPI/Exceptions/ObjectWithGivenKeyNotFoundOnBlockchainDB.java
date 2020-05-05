77deca3201f495959e41b3c633bcf84e1d0d51b0
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-05 21:53:46 +0100

    added exception class for the repository processing operation: reference to object that does not exist

---
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/FabricBlockchainRepositoryAPI/Exceptions/ObjectWithGivenKeyNotFoundOnBlockchainDB.java
@@ -0,0 +1,24 @@
+package iReceptorPlus.Blockchain.iReceptorChain.FabricBlockchainRepositoryAPI.Exceptions;
+
+public class ObjectWithGivenKeyNotFoundOnBlockchainDB extends HyperledgerFabricBlockhainRepositoryAPIException
+{
+    /**
+     * The id used to reference the traceability information.
+     */
+    String id;
+
+    public ObjectWithGivenKeyNotFoundOnBlockchainDB(String message, String id)
+    {
+        super(message);
+        this.id = id;
+    }
+
+    /**
+     * Getter to the id object.
+     * @return the id object.
+     */
+    public String getId()
+    {
+        return id;
+    }
+}
