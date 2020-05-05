9f4e7d834181155ec7b46857e6235b9558e8e2ae
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-05 20:46:32 +0100

    improved documentation on repository api class

--- fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/FabricBlockchainRepositoryAPI/HyperledgerFabricBlockhainRepositoryAPI.java
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/FabricBlockchainRepositoryAPI/HyperledgerFabricBlockhainRepositoryAPI.java
@@ -1,14 +1,17 @@
 package iReceptorPlus.Blockchain.iReceptorChain.FabricBlockchainRepositoryAPI;
 
+import com.owlike.genson.Genson;
 import iReceptorPlus.Blockchain.iReceptorChain.ChainDataTypes.iReceptorChainDataType;
 import iReceptorPlus.Blockchain.iReceptorChain.LogicDataTypes.TraceabilityDataInfo;
 import iReceptorPlus.Blockchain.iReceptorChain.LogicDataTypes.iReceptorChainDataTypeInfo;
 import org.hyperledger.fabric.contract.Context;
 
 /**
- * This class is an API that handles all calls to the hyperledger functions.
- * It is used to abstract complicated calls to the hyperledger API from the application logic.
- * The class receives the blockchain context in which this class will perform the required tasks and can then be called to perform operations on that same context without the need for interacting directly with hyperledger fabric's methods.
+ * This class is an API that handles all calls to the hyperledger functions that are necessary for CRUD operations.
+ * It is used to abstract database related calls to the hyperledger API from the application logic.
+ * The class receives the blockchain context on which it will perform the operations and can then be called to perform CRUD operations on that same context without the need for interacting directly with hyperledger fabric's methods.
+ * The class is abstract and defines the methods for CRUD operations.
+ * Subclasses will implement the specific logic necessary to handle these operations for each one of the data types.
  */
 public abstract class HyperledgerFabricBlockhainRepositoryAPI
 {
@@ -18,17 +21,26 @@ public abstract class HyperledgerFabricBlockhainRepositoryAPI
     protected Context ctx;
 
     /**
+     * The type of object that will be serialized in order to be saved in the database.
+     * Subclasses should set this attribute accordingly in order for this class to implement the serializing logic necessary for the CRUD operations.
+     */
+    Class objectType;
+
+    Genson genson = new Genson();
+
+    /**
      * Constructor for this class.
      * @param ctx The blockchain context in which this class will perform the required tasks.
+     * @param objectType The type of object that will be serialized in order to be saved in the database.
      */
-    public HyperledgerFabricBlockhainRepositoryAPI(Context ctx)
+    public HyperledgerFabricBlockhainRepositoryAPI(Context ctx, Class objectType)
     {
         this.ctx = ctx;
+        this.objectType = objectType;
     }
 
     public iReceptorChainDataTypeInfo create(iReceptorChainDataType data)
     {
-        //TODO
         return null;
     }
 
