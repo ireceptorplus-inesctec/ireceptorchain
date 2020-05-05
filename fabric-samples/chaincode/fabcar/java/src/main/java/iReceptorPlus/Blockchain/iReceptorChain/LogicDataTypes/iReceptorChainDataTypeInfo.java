e4409ef7f260f07fab9a9329bf04895930ede81a
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-05 22:04:15 +0100

    added data attrib on class iReceptorChainDataTypeInfo

--- fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/LogicDataTypes/iReceptorChainDataTypeInfo.java
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/LogicDataTypes/iReceptorChainDataTypeInfo.java
@@ -1,9 +1,26 @@
 package iReceptorPlus.Blockchain.iReceptorChain.LogicDataTypes;
 
+import iReceptorPlus.Blockchain.iReceptorChain.ChainDataTypes.TraceabilityData;
+
 public abstract class iReceptorChainDataTypeInfo
 {
     /**
      * The key used to store the data on the blockchain.
      */
     protected String key;
+
+    /**
+     * The data that this class refers to just as it is stored on chain.
+     */
+    protected iReceptorChainDataTypeInfo data;
+
+    public String getKey()
+    {
+        return key;
+    }
+
+    public iReceptorChainDataTypeInfo getData()
+    {
+        return data;
+    }
 }
