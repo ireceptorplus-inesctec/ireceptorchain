c95542273c0742cb31129addf1db684ebcad9979
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-07 01:35:29 +0100

    added definition of global config variable

--- fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChaincodeConfigs.java
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChaincodeConfigs.java
@@ -1,10 +1,14 @@
 package iReceptorPlus.Blockchain.iReceptorChain;
 
+import com.google.common.util.concurrent.AtomicDouble;
+
 import java.util.concurrent.atomic.AtomicLong;
 
 public class ChaincodeConfigs
 {
     public static AtomicLong numberOfConfirmationsNecessaryForTraceabilityInfoToBeValid = new AtomicLong(3);
+
+    public static AtomicDouble racioBetweenApprovesAndRejectionsNecessaryForTraceabilityInfoToBeValid = new AtomicDouble(0.7);
 
     private static String traceabilityAwaitingValidationKeyPrefix = "TracreabilityInfoAwaitingValidation";
 
