f805402c5c061b2ac38b2702986df5194fe98521
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-10 20:42:32 +0100

    hard coded query code to be able to test

--- fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/FabricBlockchainRepositoryAPI/HyperledgerFabricBlockhainRepositoryAPI.java
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/FabricBlockchainRepositoryAPI/HyperledgerFabricBlockhainRepositoryAPI.java
@@ -176,7 +176,7 @@ public abstract class HyperledgerFabricBlockhainRepositoryAPI
         UUID upperBoundUuid = new UUID(Long.MAX_VALUE, Long.MAX_VALUE);
         String startKey = objectTypeIdentifier + "-" + lowerBoundUuid.toString();
         String endKey = objectTypeIdentifier + "-" + upperBoundUuid.toString();
-        QueryResultsIterator<KeyValue> resultsFromStub = ctx.getStub().getStateByRange(objectTypeIdentifier + "uuid", objectTypeIdentifier +"uuid2");
+        QueryResultsIterator<KeyValue> resultsFromStub = ctx.getStub().getStateByRange(objectTypeIdentifier + "-uuid", objectTypeIdentifier +"-uuid2");
 
         ArrayList<iReceptorChainDataTypeInfo> results = new ArrayList<>();
         for (KeyValue result: resultsFromStub)
