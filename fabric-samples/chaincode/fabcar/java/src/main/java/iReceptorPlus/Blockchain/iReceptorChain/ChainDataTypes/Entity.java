a0db2e4bc64940bc50040f5f2a1e0845de3fdd7e
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-11 18:18:17 +0100

    added attrib to identify a client on Entity class

--- fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChainDataTypes/Entity.java
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChainDataTypes/Entity.java
@@ -1,5 +1,6 @@
 package iReceptorPlus.Blockchain.iReceptorChain.ChainDataTypes;
 
+import org.hyperledger.fabric.contract.ClientIdentity;
 import org.hyperledger.fabric.contract.annotation.DataType;
 
 /**
@@ -9,5 +10,14 @@ import org.hyperledger.fabric.contract.annotation.DataType;
 @DataType()
 public class Entity implements iReceptorChainDataType
 {
+    /**
+     * An instance of class ClientIdentity that hyperledger fabric uses to represent the identity of a client (peer).
+     * This contains the its certificate, an id, an msp id (id of the organization it belongs to) and may contain additional attributes created upon creation of the peers' certificate.
+     */
+    ClientIdentity clientIdentity;
 
+    public ClientIdentity getClientIdentity()
+    {
+        return clientIdentity;
+    }
 }
