2c2827c4b789ca732518d6b67a77e47a50a77d76
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-11 18:14:40 +0100

    fixed documentation on class Entity

--- fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChainDataTypes/Entity.java
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChainDataTypes/Entity.java
@@ -3,7 +3,7 @@ package iReceptorPlus.Blockchain.iReceptorChain.ChainDataTypes;
 import org.hyperledger.fabric.contract.annotation.DataType;
 
 /**
- * This class represents and entity.
+ * This class represents an entity.
  * Is used to store information about the entities who have validated a traceability information entry.
  */
 @DataType()
