e781cf3003bea5f0c9cd0cbe902557099444e114
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-11 20:15:39 +0100

    added class to contain uuid and data of an Entity

---
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/LogicDataTypes/EntityDataInfo.java
@@ -0,0 +1,11 @@
+package iReceptorPlus.Blockchain.iReceptorChain.LogicDataTypes;
+
+import iReceptorPlus.Blockchain.iReceptorChain.ChainDataTypes.iReceptorChainDataType;
+
+public class EntityDataInfo extends iReceptorChainDataTypeInfo
+{
+    public EntityDataInfo(String UUID, iReceptorChainDataType data)
+    {
+        super(UUID, data);
+    }
+}
