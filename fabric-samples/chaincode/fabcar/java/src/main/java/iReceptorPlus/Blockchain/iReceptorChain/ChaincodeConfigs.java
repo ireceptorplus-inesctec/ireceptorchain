d7e7ad005960010e2adeca7e09207f94c3e66c8b
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-11 21:31:30 +0100

    added definitions of global variables on ChainCodeConfigs regarding incentive system

--- fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChaincodeConfigs.java
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChaincodeConfigs.java
@@ -10,6 +10,13 @@ public class ChaincodeConfigs
 
     public static AtomicDouble racioBetweenApprovesAndRejectionsNecessaryForTraceabilityInfoToBeValid = new AtomicDouble(0.7);
 
+    public static AtomicLong initialReputationForEntities = new AtomicLong(100);
+
+    public static AtomicLong reputationStakeAmountNecessaryForUpVotingTraceabilityDataEntry = new AtomicLong(30);
+
+    public static AtomicLong reputationStakeAmountNecessaryForDownVotingTraceabilityDataEntry = new AtomicLong(50);
+
+
     private static String traceabilityAwaitingValidationKeyPrefix = "TracreabilityInfoAwaitingValidation";
 
     private static String traceabilityValidatedKeyPrefix = "TracreabilityInfoValidated";
