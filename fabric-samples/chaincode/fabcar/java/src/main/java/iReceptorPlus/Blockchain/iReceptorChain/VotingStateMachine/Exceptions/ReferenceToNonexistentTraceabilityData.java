4cd044d079563b0304b4cc8f2a0be5edd847cbb0
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-11 21:26:53 +0100

    renamed class

--- fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/VotingStateMachine/Exceptions/ReferenceToNonexistentTraceabilityData.java
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/VotingStateMachine/Exceptions/ReferenceToNonexistentTraceabilityData.java
@@ -4,9 +4,9 @@ package iReceptorPlus.Blockchain.iReceptorChain.VotingStateMachine.Exceptions;
  * This is a subclass that represents a specific type of exception upon processing by the state machine:
  * Reference to a nonexistent entry of traceability information by its id.
  */
-public class ReferenceToNonexistentInfo extends ReferenceToIdException
+public class ReferenceToNonexistentTraceabilityData extends ReferenceToIdException
 {
-    public ReferenceToNonexistentInfo(String id)
+    public ReferenceToNonexistentTraceabilityData(String id)
     {
         super("Reference to nonexistent traceability data information entry.", id);
     }
