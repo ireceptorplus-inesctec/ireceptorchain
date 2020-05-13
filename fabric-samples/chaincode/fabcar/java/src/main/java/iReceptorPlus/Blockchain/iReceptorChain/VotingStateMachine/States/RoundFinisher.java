a41580516f9120f4db1f5b82df05b5f70690f81e
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-13 18:20:14 +0100

    added class to perform round finishing

---
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/VotingStateMachine/States/RoundFinisher.java
@@ -0,0 +1,10 @@
+package iReceptorPlus.Blockchain.iReceptorChain.VotingStateMachine.States;
+
+/**
+ * This is a delegate class used to finish the voting round.
+ * This means to perform the necessary changes to approve of reject a traceability data entry, based on the method called.
+ * The class is called by the state classes to help performing the round closing procedures.
+ */
+public class RoundFinisher
+{
+}
