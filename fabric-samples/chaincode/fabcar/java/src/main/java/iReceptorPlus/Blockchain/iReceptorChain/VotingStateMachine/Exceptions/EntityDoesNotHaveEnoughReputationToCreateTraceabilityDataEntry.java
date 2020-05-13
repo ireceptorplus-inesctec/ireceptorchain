e5875f9ba3b26e30aed8594bb9f22da717552ea1
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-13 17:17:38 +0100

    refactored exception classes

--- fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/VotingStateMachine/Exceptions/EntityDoesNotHaveEnoughReputationToCreateTraceabilityDataEntry.java
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/VotingStateMachine/Exceptions/EntityDoesNotHaveEnoughReputationToCreateTraceabilityDataEntry.java
@@ -2,9 +2,15 @@ package iReceptorPlus.Blockchain.iReceptorChain.VotingStateMachine.Exceptions;
 
 public class EntityDoesNotHaveEnoughReputationToCreateTraceabilityDataEntry extends EntityDoesNotHaveEnoughReputationToPerformAction
 {
-
     public EntityDoesNotHaveEnoughReputationToCreateTraceabilityDataEntry(Long reputationOfEntity, Long necessaryReputationForCreating)
     {
-        super("Entity does not have enough reputation to place vote. Reputation of entity is " + reputationOfEntity + " and necessary reputation is " + necessaryReputationForCreating, reputationOfEntity, necessaryReputationForCreating);
+        super("Entity does not have enough reputation to place vote." + reputationOfEntity + " and necessary reputation is " + necessaryReputationForCreating, reputationOfEntity, necessaryReputationForCreating);
     }
+
+    @Override
+    public String getMessage()
+    {
+        return message + " Reputation of entity is " + reputationOfEntity + " and necessary reputation is " + necessaryReputation;
+    }
+
 }
--- fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/VotingStateMachine/Exceptions/EntityDoesNotHaveEnoughReputationToPlaceVote.java
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/VotingStateMachine/Exceptions/EntityDoesNotHaveEnoughReputationToPlaceVote.java
@@ -2,9 +2,14 @@ package iReceptorPlus.Blockchain.iReceptorChain.VotingStateMachine.Exceptions;
 
 public class EntityDoesNotHaveEnoughReputationToPlaceVote extends EntityDoesNotHaveEnoughReputationToPerformAction
 {
-
     public EntityDoesNotHaveEnoughReputationToPlaceVote(Long reputationOfEntity, Long necessaryReputationForVoting)
     {
-        super("Entity does not have enough reputation to place vote. Reputation of entity is " + reputationOfEntity + " and necessary reputation is " + necessaryReputationForVoting, reputationOfEntity, necessaryReputationForVoting);
+        super("Entity does not have enough reputation to place vote.", reputationOfEntity, necessaryReputationForVoting);
+    }
+
+    @Override
+    public String getMessage()
+    {
+        return message + " Reputation of entity is " + reputationOfEntity + " and necessary reputation is " + necessaryReputation;
     }
 }
