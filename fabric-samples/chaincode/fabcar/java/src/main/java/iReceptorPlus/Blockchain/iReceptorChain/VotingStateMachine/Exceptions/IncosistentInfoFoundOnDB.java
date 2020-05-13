9766690c6a0365a04de3eac7329fcda920978dc4
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-13 17:19:52 +0100

    documentation on exception class

--- fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/VotingStateMachine/Exceptions/IncosistentInfoFoundOnDB.java
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/VotingStateMachine/Exceptions/IncosistentInfoFoundOnDB.java
@@ -4,7 +4,6 @@ public class IncosistentInfoFoundOnDB extends TraceabilityInfoStateMachineExcept
 {
     /**
      * Constructor for this class that receives the message as parameter.
-     * Should be called by the subclasses and the message should be customized by them (either built or received as their own constructor parameter).
      *
      * @param message The string message that will be set as the message for the exception that occurred.
      */
