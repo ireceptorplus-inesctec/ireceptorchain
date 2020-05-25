6ecbc7ae977d8cdf5e8980eac5d2cc0eb302709d
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-25 03:32:20 +0100

    attempt to fixe all chaincode deploy errors

--- fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/iReceptorChain.java
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/iReceptorChain.java
@@ -63,6 +63,34 @@ public final class iReceptorChain implements ContractInterface {
 
     private final Genson genson = new Genson();
 
+    private enum FabCarErrors {
+        CAR_NOT_FOUND,
+        CAR_ALREADY_EXISTS
+    }
+
+    /**
+     * Retrieves a car with the specified UUID from the ledger.
+     *
+     * @param ctx the transaction context
+     * @param key the UUID
+     * @return the Car found on the ledger if there was one
+     */
+    @Transaction()
+    public Car queryCar(final Context ctx, final String key) {
+        ChaincodeStub stub = ctx.getStub();
+        String carState = stub.getStringState(key);
+
+        if (carState.isEmpty()) {
+            String errorMessage = String.format("Car %s does not exist", key);
+            System.out.println(errorMessage);
+            throw new ChaincodeException(errorMessage, FabCarErrors.CAR_NOT_FOUND.toString());
+        }
+
+        Car car = genson.deserialize(carState, Car.class);
+
+        return car;
+    }
+
     /**
      * Creates some initial Cars on the ledger.
      *
@@ -93,21 +121,218 @@ public final class iReceptorChain implements ContractInterface {
             //stub.putStringState(key, carState);
         }
 
-        createEntity(ctx, "entity");
-
-        /*
-        createTraceabilityDataEntry(ctx,
-                "uuid",
-                "inputDatasetHashValue",
-                "outputDatasetHashValue",
-                "softwareId",
-                "softwareVersion",
-                "softwareBinaryExecutableHashValue",
-                "softwareConfigParams"
-                );*/
+  
     }
 
     /**
+     * Creates a new car on the ledger.
+     *
+     * @param ctx the transaction context
+     * @param key the UUID for the new car
+     * @param make the make of the new car
+     * @param model the model of the new car
+     * @param color the color of the new car
+     * @param owner the owner of the new car
+     * @return the created Car
+     */
+    @Transaction()
+    public Car createCar(final Context ctx, final String key, final String make, final String model,
+            final String color, final String owner) {
+        ChaincodeStub stub = ctx.getStub();
+
+        String carState = stub.getStringState(key);
+        if (!carState.isEmpty()) {
+            String errorMessage = String.format("Car %s already exists", key);
+            System.out.println(errorMessage);
+            throw new ChaincodeException(errorMessage, FabCarErrors.CAR_ALREADY_EXISTS.toString());
+        }
+
+        Car car = new Car(make, model, color, owner);
+        carState = genson.serialize(car);
+        stub.putStringState(key, carState);
+
+        return car;
+    }
+
+    /**
+     * Retrieves every car between CAR0 and CAR999 from the ledger.
+     *
+     * @param ctx the transaction context
+     * @return array of Cars found on the ledger
+     */
+    @Transaction()
+    public CarQueryResult[] queryAllCars(final Context ctx) {
+
+        ChaincodeStub stub = ctx.getStub();
+
+        final String startKey = "CAR0";
+        final String endKey = "CAR999";
+        List<CarQueryResult> queryResults = new ArrayList<CarQueryResult>();
+
+        QueryResultsIterator<KeyValue> results = stub.getStateByRange(startKey, endKey);
+
+        for (KeyValue result: results) {
+            Car car = genson.deserialize(result.getStringValue(), Car.class);
+            queryResults.add(new CarQueryResult(result.getKey(), car));
+        }
+
+        CarQueryResult[] response = queryResults.toArray(new CarQueryResult[queryResults.size()]);
+
+
+        return response;
+    }
+
+    private EntityID getEntityIdFromContext(Context ctx)
+    {
+        return new EntityID(ctx.getClientIdentity().getId());
+    }
+
+    @Transaction()
+    public TraceabilityData createTraceabilityEntries(final Context ctx) {
+        iReceptorChainDataType traceabilityDataAwatingValidation = new TraceabilityDataAwatingValidation("a","b", new ProcessingDetails("", "", "", ""), new EntityID("a"));
+        iReceptorChainDataType traceabilityDataValidated = new TraceabilityDataValidated("c","d", new ProcessingDetails("", "", "", ""), new EntityID("a"), new ArrayList<>(), new ArrayList<>());
+
+        ChaincodeStub stub = ctx.getStub();
+
+        String traceabilityInfoAwatingValidationState = genson.serialize(traceabilityDataAwatingValidation);
+        String traceabilityInfoValidatedState = genson.serialize(traceabilityDataValidated);
+        stub.putStringState("traceabilityInfoAwatingValidation5", traceabilityInfoAwatingValidationState);
+        stub.putStringState("traceabilityInfoValidated5", traceabilityInfoValidatedState);
+
+        return null;
+
+    }
+
+    @Transaction()
+    public void testQueryTraceability(final Context ctx) {
+        ChaincodeStub stub = ctx.getStub();
+
+        List<TraceabilityData> queryResults = new ArrayList<TraceabilityData>();
+
+        String traceabilityInfoAwatingValidation = stub.getStringState("traceabilityInfoAwatingValidation5");
+        System.err.println("*******traceabilityInfoAwatingValidation******");
+        System.err.println(traceabilityInfoAwatingValidation);
+        if (traceabilityInfoAwatingValidation.isEmpty())
+            System.err.println("does not exist1");
+        else
+            System.err.println("exists1");
+        TraceabilityData resultsAwatingValidation = genson.deserialize(traceabilityInfoAwatingValidation, TraceabilityDataAwatingValidation.class);
+        String traceabilityInfoValidated = stub.getStringState("traceabilityInfoValidated5");
+        System.err.println("******traceabilityInfoValidated********");
+        System.err.println(traceabilityInfoValidated);
+        if (traceabilityInfoValidated.isEmpty())
+            System.err.println("does not exist2");
+        TraceabilityData resultsValidated = genson.deserialize(traceabilityInfoValidated, TraceabilityDataValidated.class);
+
+
+        resultsAwatingValidation.registerYesVoteForValidity(getEntityIdFromContext(ctx));
+        resultsValidated.registerYesVoteForValidity(getEntityIdFromContext(ctx));
+        System.err.println("stuff4");
+
+
+    }
+
+    @Transaction()
+    public String testClientIdentity(final Context ctx)
+    {
+        ClientIdentity clientIdentity = ctx.getClientIdentity();
+        System.err.println("client id: " + clientIdentity.getId());
+        System.err.println("client msp id: " + clientIdentity.getMSPID());
+        byte[] creatorBytes = ctx.getStub().getCreator();
+        String creatorBytesBase64 = Base64.getEncoder().encodeToString(creatorBytes);
+        return creatorBytesBase64;
+    }
+
+    @Transaction()
+    public EntityData testNotFinalAttribs(final Context ctx)
+    {
+        return new EntityData(ctx.getClientIdentity().getId());
+    }
+
+    @Transaction()
+    public TraceabilityData test(final Context ctx) {
+        ChaincodeStub stub = ctx.getStub();
+
+/*
+        Car car = new Car("makeeee", "modefl", "colfdor", "owndfer");
+        String carState = genson.serialize(car);
+        System.err.println("carState");
+        System.err.println("carState");
+        System.err.println("carState");
+        System.err.println("carState");
+        System.err.println("carState");
+        System.err.println(carState);
+        System.err.println("carState end");
+        System.err.println("carState end");
+        System.err.println("carState end");
+        System.err.println("carState end");
+        System.err.println("carState end");
+        stub.putStringState("carr1", car);
+*/
+
+        stub.putStringState("carr1", "stuffffff");
+
+        String carStateQueryResult = stub.getStringState("carr1");
+        if (carStateQueryResult.isEmpty())
+            System.err.println("does not exist0");
+        else
+            System.err.println("exists0 and result is: " + carStateQueryResult);
+
+        String traceabilityInfoAwatingValidation = stub.getStringState("traceabilityInfoAwatingValidation5");
+        System.err.println("traceabilityInfoAwatingValidation");
+        System.err.println(traceabilityInfoAwatingValidation);
+        if (traceabilityInfoAwatingValidation.isEmpty())
+            System.err.println("does not exist1");
+        else
+            System.err.println("exists1");
+        iReceptorChainDataType resultsAwatingValidation = genson.deserialize(traceabilityInfoAwatingValidation, TraceabilityDataAwatingValidation.class);
+        String traceabilityInfoValidated = stub.getStringState("traceabilityInfoValidated5");
+        System.err.println("traceabilityInfoValidated");
+        System.err.println(traceabilityInfoValidated);
+        if (traceabilityInfoValidated.isEmpty())
+            System.err.println("does not exist2");
+        iReceptorChainDataType resultsValidated = genson.deserialize(traceabilityInfoValidated, TraceabilityDataAwatingValidation.class);
+
+        return (TraceabilityData) resultsValidated;
+/*
+        resultsAwatingValidation.registerYesVoteForValidity(getEntityIdFromContext(ctx));
+        resultsValidated.registerYesVoteForValidity(getEntityIdFromContext(ctx));
+        System.err.println("stuff4");
+*/
+
+    }
+
+
+    /**
+     * Changes the owner of a car on the ledger.
+     *
+     * @param ctx the transaction context
+     * @param key the UUID
+     * @param newOwner the new owner
+     * @return the updated Car
+     */
+    @Transaction()
+    public Car changeCarOwner(final Context ctx, final String key, final String newOwner) {
+        ChaincodeStub stub = ctx.getStub();
+
+        String carState = stub.getStringState(key);
+
+        if (carState.isEmpty()) {
+            String errorMessage = String.format("Car %s does not exist", key);
+            System.out.println(errorMessage);
+            throw new ChaincodeException(errorMessage, FabCarErrors.CAR_NOT_FOUND.toString());
+        }
+
+        Car car = genson.deserialize(carState, Car.class);
+
+        Car newCar = new Car(car.getMake(), car.getModel(), car.getColor(), newOwner);
+        String newCarState = genson.serialize(newCar);
+        stub.putStringState(key, newCarState);
+
+        return newCar;
+    }
+
+     /**
      * Creates a new entity on the ledger.
      *
      * @param ctx the transaction context
@@ -116,34 +341,11 @@ public final class iReceptorChain implements ContractInterface {
      */
     @Transaction()
     public EntityDataInfo createEntity(final Context ctx, final ClientIdentity clientIdentity) {
-
-        return createEntity(ctx, clientIdentity.getId());
-    }
-
-    /**
-     * Returns the entity that has called the Transaction, based on the context.
-     * @param ctx The context.
-     * @return An instance of EntityID representing the entity that has called a given transaction.
-     */
-    private EntityID getEntityIdFromContext(Context ctx)
-    {
-        return new EntityID(ctx.getClientIdentity().getId());
-    }
-
-    /**
-     * Creates a new entity on the ledger.
-     *
-     * @param ctx the transaction context
-     * @param entityID A string representing the id of the entity.
-     * @return the EntityData entry just created on the blockchain.
-     */
-    @Transaction()
-    public EntityDataInfo createEntity(final Context ctx, final String entityID) {
         logDebugMsg("createTraceabilityDataEntry");
 
         ChaincodeStub stub = ctx.getStub();
 
-        EntityData entityData = new EntityData(entityID);
+        EntityData entityData = new EntityData(clientIdentity.getId());
         EntityDataInfo entityDataInfo = new EntityDataInfo(entityData.getId(), entityData);
         EntityDataRepositoryAPI api = new EntityDataRepositoryAPI(ctx);
         try
@@ -155,22 +357,6 @@ public final class iReceptorChain implements ContractInterface {
         }
 
         return entityDataInfo;
-    }
-
-    /**
-     * Creates a new traceability data entry on the ledger.
-     * The entry is placed on the pool of traceability data's waiting to be validated by peers.
-     * @param ctx the transaction context
-     * @param newUUID the new UUID of the object to be created. This is generated at client-side in order to avoid different blockchain nodes reaching different ids for the same transaction for a creation of an object.
-     * @param traceabilityData An instance of class TraceabilityDataAwatingValidation containing the traceability data to be inserted in the blockchain.
-     */
-    @Transaction()
-    public TraceabilityDataAwatingValidationReturnType createTraceabilityDataEntry(final Context ctx, final String newUUID, final TraceabilityDataAwatingValidation traceabilityData)
-    {
-        ProcessingDetails processingDetails = traceabilityData.getProcessingDetails();
-        return createTraceabilityDataEntry(ctx, newUUID, traceabilityData.getInputDatasetHashValue(), traceabilityData.getOutputDatasetHashValue(),
-                processingDetails.getSoftwareId(), processingDetails.getSoftwareVersion(), processingDetails.getSoftwareBinaryExecutableHashValue(),
-                processingDetails.getSoftwareConfigParams());
     }
 
     /**
@@ -363,7 +549,7 @@ public final class iReceptorChain implements ContractInterface {
 
         return response;
     }
-
+    
     /**
      * Retrieves all traceability data that is in state awating validation.
      *
