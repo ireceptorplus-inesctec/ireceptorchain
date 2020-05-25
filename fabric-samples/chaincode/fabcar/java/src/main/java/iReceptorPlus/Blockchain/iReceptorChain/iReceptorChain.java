9d09b90280ebf3fce32df87bcc1ef515e2fe617c
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-05-25 17:10:34 +0100

    fixed creator entity's id on initLedger

--- fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/iReceptorChain.java
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/iReceptorChain.java
@@ -121,7 +121,7 @@ public final class iReceptorChain implements ContractInterface {
             //stub.putStringState(key, carState);
         }
 
-        createEntityById(ctx, "creator");
+        createEntityById(ctx, "x509::CN=org2admin, OU=admin, O=Hyperledger, ST=North Carolina, C=US::CN=fabric-ca-server, OU=Fabric, O=Hyperledger, ST=North Carolina, C=US");
 
         createTraceabilityDataEntry(ctx,
                 "uuid",
