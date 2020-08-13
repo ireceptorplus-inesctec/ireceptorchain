ceebbe7a3a0b6473658a473574454dcadbcee465
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-08-13 17:27:52 +0100

    changed test script for creating traceability data entry

--- fabric-samples/fabcar/javascript/query.js
+++ fabric-samples/fabcar/javascript/query.js
@@ -43,7 +43,7 @@ async function main() {
         // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
         //const result = await contract.evaluateTransaction('createCar', 'CARRRR', 'make', 'model', 'color', 'owner');
         //const result = await contract.evaluateTransaction('test');
-        /*const createTraceabilityEntryResult = await contract.evaluateTransaction('createTraceabilityDataEntry',
+        const createTraceabilityEntryResult = await contract.evaluateTransaction('createTraceabilityDataEntry',
         'uuid',
         'inputDatasetHashValue',
         'outputDatasetHashValue',
@@ -51,7 +51,7 @@ async function main() {
         'softwareVersion',
         'softwareBinaryExecutableHashValue',
         'softwareConfigParams',
-        );*/
+        );
         const registerYesVoteForTraceabilityEntryInVotingRoundResult = await contract.evaluateTransaction('registerYesVoteForTraceabilityEntryInVotingRound',
         'uuid',
         );
