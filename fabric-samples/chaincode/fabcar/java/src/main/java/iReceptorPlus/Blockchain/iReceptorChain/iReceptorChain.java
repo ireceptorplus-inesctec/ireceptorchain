16975c4e21d315791bfbb89ad99b469d8449a49b
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-10-12 02:01:43 +0100

    fixed test case

--- fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/iReceptorChain.java
+++ fabric-samples/chaincode/fabcar/java/src/main/java/iReceptorPlus/Blockchain/iReceptorChain/iReceptorChain.java
@@ -263,7 +263,7 @@ public final class iReceptorChain implements ContractInterface {
     }
 
     @Transaction()
-    public String createMockTraceabilityData(final Context ctx) {
+    public String createMockTraceabilityData(final Context ctx, final String uuid) {
         ChaincodeStub stub = ctx.getStub();
 
 /*
@@ -316,7 +316,7 @@ public final class iReceptorChain implements ContractInterface {
         TraceabilityDataAwaitingValidationRepositoryAPI api = new TraceabilityDataAwaitingValidationRepositoryAPI(ctx);
         try
         {
-            api.create("uuid", new TraceabilityDataAwaitingValidation("", "", new ProcessingDetails("", "", "", ""), new EntityID("entity")));
+            api.create(uuid, new TraceabilityDataAwaitingValidation("", "", new ProcessingDetails("", "", "", ""), new EntityID("entity")));
         } catch (GivenIdIsAlreadyAssignedToAnotherObject givenIdIsAlreadyAssignedToAnotherObject)
         {
             throw new ChaincodeException("not able to create");
@@ -326,12 +326,12 @@ public final class iReceptorChain implements ContractInterface {
     }
 
     @Transaction()
-    public String readMockTraceabilityData(final Context ctx)
+    public String readMockTraceabilityData(final Context ctx, final String uuid)
     {
         TraceabilityDataAwaitingValidationRepositoryAPI api = new TraceabilityDataAwaitingValidationRepositoryAPI(ctx);
         try
         {
-            api.read("uuid");
+            api.read(uuid);
         } catch (ObjectWithGivenKeyNotFoundOnBlockchainDB objectWithGivenKeyNotFoundOnBlockchainDB)
         {
             throw new ChaincodeException("read failed");
