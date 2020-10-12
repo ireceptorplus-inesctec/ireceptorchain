4b55d52d0be74852fd1eb4872a0bab57f55e08df
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2020-10-12 01:22:29 +0100

    debugging on query.js

--- fabric-samples/fabcar/javascript/query.js
+++ fabric-samples/fabcar/javascript/query.js
@@ -61,7 +61,7 @@ async function main() {
         //const result1 = await contract.submitTransaction('createMockTraceabilityData');
         //console.log(result1);
         console.log("stuff2");
-        /*const createTraceabilityEntryResult = await contractAdmin.submitTransaction('createTraceabilityDataEntry',
+        const createTraceabilityEntryResult = await contractAdmin.submitTransaction('createTraceabilityDataEntry',
         "uuid",
         'inputDatasetHashValue',
         'outputDatasetHashValue',
@@ -69,15 +69,15 @@ async function main() {
         'softwareVersion',
         'softwareBinaryExecutableHashValue',
         'softwareConfigParams',
-        );       */
+        );       
         console.log("stuff3");
         //const result2 = await contract.submitTransaction('testVote');
         //console.log(result2.toString());
         console.log("stuff4");
-        const createMockTraceabilityDataResult = await contract.evaluateTransaction('createMockTraceabilityData',
+        *-const createMockTraceabilityDataResult = await contract.evaluateTransaction('createMockTraceabilityData',
         'uuid',
         );
-        console.log(`createMockTraceabilityDataResult: ${createMockTraceabilityDataResult.toString()}`);
+        console.log(`createMockTraceabilityDataResult: ${createMockTraceabilityDataResult.toString()}`);-*
 
         console.log("stuff5");
         const getAllAwaitingValidationTraceabilityDataEntries = await contract.evaluateTransaction('getAllAwaitingValidationTraceabilityDataEntries',
