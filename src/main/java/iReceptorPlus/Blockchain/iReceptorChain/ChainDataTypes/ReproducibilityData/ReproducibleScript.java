ddc7d0148d0cd5f3980e87673455e77e50076b49
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2021-09-02 01:33:28 +0100

    added class to represent a script that can be run to reproduce a processing

---
+++ src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChainDataTypes/ReproducibilityData/ReproducibleScript.java
@@ -0,0 +1,12 @@
+package iReceptorPlus.Blockchain.iReceptorChain.ChainDataTypes.ReproducibilityData;
+
+import org.hyperledger.fabric.contract.annotation.Property;
+
+public abstract class ReproducibleScript
+{
+    /**
+     * The URL from which the script can be fetched.
+     */
+    @Property()
+    DatasetURL url;
+}
