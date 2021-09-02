e53200a937762cce616cdae108f3294e2e69e9c0
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2021-09-02 01:37:09 +0100

    added missing @Property annotations on class fields

--- src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChainDataTypes/ReproducibilityData/DatasetURL.java
+++ src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChainDataTypes/ReproducibilityData/DatasetURL.java
@@ -1,5 +1,17 @@
 package iReceptorPlus.Blockchain.iReceptorChain.ChainDataTypes.ReproducibilityData;
 
+import org.hyperledger.fabric.contract.annotation.Property;
+
 public class DatasetURL implements DatasetSource
 {
+    /**
+     * A string representing the URL from which the script can be fetched.
+     */
+    @Property()
+    String url;
+
+    public String getUrl()
+    {
+        return url;
+    }
 }
--- src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChainDataTypes/ReproducibilityData/ReproducibilityData.java
+++ src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChainDataTypes/ReproducibilityData/ReproducibilityData.java
@@ -1,5 +1,7 @@
 package iReceptorPlus.Blockchain.iReceptorChain.ChainDataTypes.ReproducibilityData;
 
+import org.hyperledger.fabric.contract.annotation.Property;
+
 import javax.xml.crypto.Data;
 import java.util.ArrayList;
 
@@ -8,16 +10,19 @@ public class ReproducibilityData
     /**
      * The source from which the input dataset(s) can be fetched so that the processing may be performed.
      */
+    @Property()
     ArrayList<DatasetSource> inputDatasets;
 
     /**
      * An executable script that allows to reproduce the processing made to the data.
      */
+    @Property()
     ReproducibleScript script;
 
     /**
      * The source from which the output dataset(s) can be fetched to validate the output of the processing.
      */
+    @Property()
     ArrayList<DatasetSource> outputDatasets;
 
     public ReproducibilityData(ArrayList<DatasetSource> inputDatasets, ReproducibleScript script, ArrayList<DatasetSource> outputDatasets)
