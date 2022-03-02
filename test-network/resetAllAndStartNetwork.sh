1842d749863fb3850e38e28dcfc0d6da5e86e771
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-03-02 15:25:25 +0000

    added bash script to reset all and setup the test network from scratch

---
+++ test-network/resetAllAndStartNetwork.sh
@@ -0,0 +1,26 @@
+docker system prune -a --force
+docker volume prune --force
+cd ..
+./gradlew installDist
+cd test-network
+export PATH=${PWD}/../bin:$PATH
+export FABRIC_CFG_PATH=$PWD/../config/
+peer lifecycle chaincode package ireceptorchain.tar.gz --path ../build/install/ireceptorchain --lang java --label ireceptorchain_1
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+
+peer lifecycle chaincode install ireceptorchain.tar.gz
+
+export CORE_PEER_LOCALMSPID="Org2MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+export CORE_PEER_ADDRESS=localhost:9051
+
+peer lifecycle chaincode install ireceptorchain.tar.gz
+
+peer lifecycle chaincode queryinstalled
