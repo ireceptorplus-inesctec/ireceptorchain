8830a3022e6de94d7661e9e297804b92e8cf2f49
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-03-02 15:50:01 +0000

    added missing command to bash script

--- test-network/resetAllAndStartNetwork.sh
+++ test-network/resetAllAndStartNetwork.sh
@@ -1,3 +1,4 @@
+./network.sh down
 docker system prune -a --force
 docker volume prune --force
 cd ..
@@ -7,6 +8,7 @@ cd ireceptorchain
 curl -sSL https://bit.ly/2ysbOFE | bash -s
 ./gradlew installDist
 cd test-network
+./network.sh up createChannel
 export PATH=${PWD}/../bin:$PATH
 export FABRIC_CFG_PATH=$PWD/../config/
 peer lifecycle chaincode package ireceptorchain.tar.gz --path ../build/install/ireceptorchain --lang java --label ireceptorchain_1
@@ -28,3 +30,8 @@ export CORE_PEER_ADDRESS=localhost:9051
 peer lifecycle chaincode install ireceptorchain.tar.gz
 
 peer lifecycle chaincode queryinstalled
+
+export CC_PACKAGE_ID=ireceptorchain_1:2759e99b93dede71e9af1336da9859fffc9ed0d79363bdd930239d1a4049e6af
+
+peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name ireceptorchain --version 1.0 --package-id $CC_PACKAGE_ID --sequence 1 --tls --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+
