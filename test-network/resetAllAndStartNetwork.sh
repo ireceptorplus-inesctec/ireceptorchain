f0d9238dfffbde154899007f93b898181664d0fb
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-03-03 20:52:48 +0000

    replaced all instructions with simple one to deploy CC

--- test-network/resetAllAndStartNetwork.sh
+++ test-network/resetAllAndStartNetwork.sh
@@ -9,38 +9,6 @@ cd ireceptorchain
 curl -sSL https://bit.ly/2ysbOFE | bash -s
 ./gradlew installDist
 cd test-network
-./network.sh up createChannel
-export PATH=${PWD}/../bin:$PATH
-export FABRIC_CFG_PATH=$PWD/../config/
-peer lifecycle chaincode package ireceptorchain.tar.gz --path ../build/install/ireceptorchain --lang java --label ireceptorchain_1
-
-export CORE_PEER_TLS_ENABLED=true
-export CORE_PEER_LOCALMSPID="Org1MSP"
-export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-export CORE_PEER_ADDRESS=localhost:7051
-
-peer lifecycle chaincode install ireceptorchain.tar.gz
-
-export CORE_PEER_LOCALMSPID="Org2MSP"
-export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
-export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
-export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
-export CORE_PEER_ADDRESS=localhost:9051
-
-peer lifecycle chaincode install ireceptorchain.tar.gz
-
-peer lifecycle chaincode queryinstalled
-
-export CC_PACKAGE_ID=ireceptorchain_1:2759e99b93dede71e9af1336da9859fffc9ed0d79363bdd930239d1a4049e6af
-
-peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name ireceptorchain --version 1.0 --package-id $CC_PACKAGE_ID --sequence 1 --tls --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
-
-export CORE_PEER_LOCALMSPID="Org1MSP"
-export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-export CORE_PEER_ADDRESS=localhost:7051
-
-peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name ireceptorchain --version 1.0 --package-id $CC_PACKAGE_ID --sequence 1 --tls --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
-
-peer lifecycle chaincode checkcommitreadiness --channelID mychannel --name ireceptorchain --version 1.0 --sequence 1 --tls --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem --output json --signature-policy "OR ('Org1MSP.peer','Org2MSP.peer')"
+./network.sh down
+./network.sh up createChannel -ca -s couchdb
+./network.sh deployCC -ccn ireceptorchain -ccv 1 -cci initLedger -ccl java -ccp ../
