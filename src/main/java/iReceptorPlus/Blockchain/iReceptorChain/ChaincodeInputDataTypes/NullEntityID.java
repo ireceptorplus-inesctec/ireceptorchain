cffd5e16b97cd3433b1086c3631d7c63c2e98919
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-03-30 00:43:16 +0100

    added chaincode input DTOs

---
+++ src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChaincodeInputDataTypes/NullEntityID.java
@@ -0,0 +1,11 @@
+package iReceptorPlus.Blockchain.iReceptorChain.ChaincodeInputDataTypes;
+
+import iReceptorPlus.Blockchain.iReceptorChain.ChainDataTypes.EntityID;
+
+public class NullEntityID extends EntityID
+{
+    public NullEntityID()
+    {
+        super("Null Entity");
+    }
+}
