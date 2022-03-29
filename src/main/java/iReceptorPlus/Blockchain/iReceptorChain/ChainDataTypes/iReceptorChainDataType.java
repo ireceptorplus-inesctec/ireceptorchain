c0ffc22c72586849cc3d7d17939ced2660df1b83
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-03-29 17:47:52 +0100

    added documentation to interface classes

--- src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChainDataTypes/iReceptorChainDataType.java
+++ src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChainDataTypes/iReceptorChainDataType.java
@@ -1,5 +1,8 @@
 package iReceptorPlus.Blockchain.iReceptorChain.ChainDataTypes;
 
+/**
+ * Represents a class that is stored on the blockchain database.
+ */
 public interface iReceptorChainDataType
 {
 
--- src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChaincodeReturnDataTypes/ChaincodeReturnDataType.java
+++ src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChaincodeReturnDataTypes/ChaincodeReturnDataType.java
@@ -5,6 +5,10 @@ import org.hyperledger.fabric.contract.annotation.Property;
 
 import java.util.Objects;
 
+/**
+ * Represents a class that is returned to the client of the blockchain.
+ * There is a subclass of this class which is a DTO version of every class on package ChainDataTypes.
+ */
 public abstract class ChaincodeReturnDataType
 {
 
