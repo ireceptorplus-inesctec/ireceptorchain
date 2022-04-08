8bfbe0b8010a696d8fa104b5d5bbcb2db39b462a
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-04-08 11:33:20 +0100

    added missing annotations to VoteResultReturnType to fix NullPointerException

--- src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChaincodeReturnDataTypes/VoteResultReturnType.java
+++ src/main/java/iReceptorPlus/Blockchain/iReceptorChain/ChaincodeReturnDataTypes/VoteResultReturnType.java
@@ -1,20 +1,27 @@
 package iReceptorPlus.Blockchain.iReceptorChain.ChaincodeReturnDataTypes;
 
+import com.owlike.genson.annotation.JsonProperty;
+import org.hyperledger.fabric.contract.annotation.DataType;
+import org.hyperledger.fabric.contract.annotation.Property;
+
 import java.util.Objects;
 
+@DataType()
 public class VoteResultReturnType extends ChaincodeReturnDataType
 {
     /**
      * A message describing the return.
      */
+    @Property()
     String message;
 
     /**
      * A boolean identifying whether the execution has caused the traceability information to change its state.
      */
+    @Property()
     boolean stateChange;
 
-    public VoteResultReturnType(String message, boolean stateChange)
+    public VoteResultReturnType(@JsonProperty("message") String message, @JsonProperty("stateChange") boolean stateChange)
     {
         this.message = message;
         this.stateChange = stateChange;
